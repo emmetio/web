52ed332e3056748aa588586a95286791eececbbc
Sergey Chikuyonok <serge.che@gmail.com>
2013-02-05 00:34:14 +0400

    Cross-browser support

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -1,3 +1,10 @@
+(function() {
+	// detect SVG support
+	var svgNS = 'http://www.w3.org/2000/svg';
+	var hasSVG = !!document.createElementNS && !!document.createElementNS(svgNS, 'svg').createSVGRect;
+	$(document.documentElement).addClass(hasSVG ? 'svg' : 'no-svg');
+})();
+
 // A super-hacky way to to change the way how Disqus
 // outputs comments count labels
 $(function() {
@@ -8,9 +15,11 @@ $(function() {
 			.toLowerCase();
 	}
 
-	$('a').each(function(i, el) {
-		if (~el.href.indexOf('#disqus_thread')) {
-			el.addEventListener('DOMSubtreeModified', callback);
-		}
-	});
+	if (document.addEventListener) {
+		$('a').each(function(i, el) {
+			if (~el.href.indexOf('#disqus_thread')) {
+				el.addEventListener('DOMSubtreeModified', callback);
+			}
+		});
+	}
 });
