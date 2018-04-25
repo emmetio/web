fcb08c0c9ce8630fe92222b82e832ed47933678a
Sergey Chikuyonok <serge.che@gmail.com>
2018-04-26 01:56:26 +0300

    Unit-test for abbreviation syntax mode

--- src/lib/markup-abbreviation-mode.js
+++ src/lib/markup-abbreviation-mode.js
@@ -16,65 +16,75 @@ const reverseBraces = {
  * Emmet abbreviation parsing mode
  */
 export default function() {
+	return { startState, token };
+}
+
+/**
+ * Returns initial parsing state
+ */
+function startState() {
 	return {
-		startState() {
-			return {
-				braces: [],
-				attr: 0,
-				quote: null,
-				parseError: null
-			};
-		},
-		token(stream, state) {
-			// Handle braces first to exit text or attribute context as soon as possible
-			if (stream.peek() in braces) {
-				state.braces.push(stream.next());
-				return 'bracket';
-			} else if (stream.peek() in reverseBraces) {
-				if (last(state.braces) === reverseBraces[stream.next()]) {
-					state.braces.pop();
-					return 'bracket';
-				}
-
-				return 'bracket error';
-			}
-
-			if (inAttribute(state)) {
-				return parseAttribute(stream, state);
-			}
-
-			if (inText(state)) {
-				return parseText(stream);
-			}
-
-			if (stream.eatWhile(ident)) {
-				return 'tag';
-			}
-
-			const ch = stream.next();
-
-			if (ch === '.') {
-				stream.eatWhile(ident);
-				return 'attribute class';
-			}
-
-			if (ch === '#') {
-				stream.eatWhile(ident);
-				return 'attribute id';
-			}
-
-			if (ch === '*') {
-				stream.eatWhile(num);
-				return 'number';
-			}
-
-			if (ch === '+' || ch === '>' || ch === '^' || ch === '/') {
-				return 'operator';
-			}
-
-			return unexpectedCharacter(stream, state);
-		}
+		braces: [],
+		attr: 0,
+		quote: null,
+		parseError: null
 	};
+}
+
+/**
+ * Consumes token from given stream
+ * @param {CodeMirror.StringStream} stream
+ * @param {Object} state
+ * @returns {String}
+ */
+function token(stream, state) {
+	if (inAttribute(state)) {
+		return exitBraceState(stream, state, ']') || parseAttribute(stream, state);
+	}
+
+	if (inText(state)) {
+		return exitBraceState(stream, state, '}') || parseText(stream);
+	}
+
+	// Handle braces first to exit text or attribute context as soon as possible
+	if (stream.peek() in braces) {
+		state.braces.push(stream.next());
+		return 'bracket';
+	} else if (stream.peek() in reverseBraces) {
+		if (last(state.braces) === reverseBraces[stream.next()]) {
+			state.braces.pop();
+			return 'bracket';
+		}
+
+		return 'bracket error';
+	}
+
+	if (stream.eatWhile(ident)) {
+		return 'tag';
+	}
+
+	const ch = stream.next();
+
+	if (ch === '.') {
+		stream.eatWhile(ident);
+		return 'attribute class';
+	}
+
+	if (ch === '#') {
+		stream.eatWhile(ident);
+		return 'attribute id';
+	}
+
+	if (ch === '*') {
+		stream.eatWhile(num);
+		return 'number';
+	}
+
+	if (ch === '+' || ch === '>' || ch === '^' || ch === '/') {
+		return 'operator';
+	}
+
+	return unexpectedCharacter(stream, state);
 }
 
 function ident(ch) {
@@ -161,6 +171,11 @@ function parseAttribute(stream, state) {
 			return null;
 		}
 
+		if (stream.eat('!')) {
+			// implied default attribute
+			return 'operator';
+		}
+
 		if (token = consumeQuoted(stream, state)) {
 			// Consumed quoted value: anonymous attribute
 			return `${token} ${attrValue}`;
@@ -168,8 +183,13 @@ function parseAttribute(stream, state) {
 
 		if (token = consumeUnquoted(stream, state)) {
 			// Consumed next word: could be either attribute name or unquoted default value
-			if (reAttributeName.test(stream.current())) {
+			const value = stream.current();
+			if (reAttributeName.test(value)) {
 				// Attribute name
+				if (value[value.length - 1] === '.') {
+					// Default attribute, delegate parsing of `.` to next call
+					stream.backUp(1);
+				}
 				state.attr++; // expect =
 				return 'attribute attribute-name';
 			}
@@ -179,8 +199,20 @@ function parseAttribute(stream, state) {
 		}
 	} else if (state.attr === 1) {
 		if (stream.eat('=')) {
-			state.attr++;
+			// Check for unexpected (but valid) empty attribute value
+			if (/\s|\]/.test(stream.peek())) {
+				state.attr = 0;
+			} else {
+				state.attr++;
+			}
+
 			return null;
+		}
+
+		if (stream.eat('.')) {
+			// Boolean attribute
+			state.attr = 0;
+			return 'operator';
 		}
 
 		// looks like next attribute
@@ -189,18 +221,14 @@ function parseAttribute(stream, state) {
 	} else if (state.attr === 2) {
 		// Expect attribute value after '='
 		state.attr = 0;
-		token = consumeQuoted(stream, state) || consumeUnquoted(stream, state);
-		if (token) {
+		if (token = reactExpression(stream, state) || consumeQuoted(stream, state) || consumeUnquoted(stream, state)) {
 			return `${token} ${attrValue}`;
 		}
 	}
 
 	// Unexpected state
 	state.attr = 0;
-	state.parseError = {
-		message: 'Expected attribute value',
-		ch: state.pos
-	};
+	state.parseError = error('Expected attribute value', stream);
 }
 
 /**
@@ -229,19 +257,54 @@ function parseText(stream) {
 	return 'text';
 }
 
-function unexpectedCharacter(stream, state) {
-	if (!state.parseError) {
-		state.parseError = {
-			message: 'Unexpected character',
-			ch: stream.pos
-		};
+/**
+ * Consumes React-like expression: an attribute value in curly braces
+ * @param {CodeMirror.StringStream} stream
+ */
+function reactExpression(stream, state) {
+	if (stream.eat('{')) {
+		let stack = 1, ch;
+
+		while (!stream.eol()) {
+			ch = stream.next();
+			if (ch === '{') {
+				stack++;
+			} else if (ch === '}') {
+				if (--stack === 0) {
+					return 'variable-3';
+				}
+			} else if (ch === '\\') {
+				stream.next();
+			}
+		}
+
+		state.parseError = error('Expecting closing }', stream);
+
+		return 'error';
 	}
+}
+
+function unexpectedCharacter(stream, state) {
+	state.parseError = error('Unexpected character', stream);
 
 	while (!stream.eol()) {
 		stream.next();
 	}
 
 	return 'error';
+}
+
+function exitBraceState(stream, state, brace) {
+	if (stream.eat(brace)) {
+		state.braces.pop();
+		return 'bracket';
+	}
+}
+
+function error(message, stream) {
+	const err = new SyntaxError(message);
+	err.ch = stream.pos;
+	return err;
 }
 
 function last(arr) {
--- test/syntax-modes.js
+++ test/syntax-modes.js
@@ -39,15 +39,310 @@ describe('Snippet Name mode', () => {
 	});
 });
 
+// These are unit-test samples from Emmet abbreviation parser
 describe('Abbreviation mode', () => {
 	const parse = str => _parse(str, abbreviationMode());
 
-	it('parse', () => {
+	it('simple', () => {
 		assert.deepEqual(parse('foo'), [['foo', 'tag']]);
 		assert.deepEqual(parse('foo.bar'), [['foo', 'tag'], ['.bar', 'attribute class']]);
 		assert.deepEqual(parse('foo#bar'), [['foo', 'tag'], ['#bar', 'attribute id']]);
 		assert.deepEqual(parse('.foo#bar'), [['.foo', 'attribute class'], ['#bar', 'attribute id']]);
+		assert.deepEqual(parse('.foo.bar'), [['.foo', 'attribute class'], ['.bar', 'attribute class']]);
+		assert.deepEqual(parse('.foo_bar'), [['.foo_bar', 'attribute class']]);
+		assert.deepEqual(parse('.'), [['.', 'attribute class']]);
+		assert.deepEqual(parse('.#'), [['.', 'attribute class'], ['#', 'attribute id']]);
+	});
 
-		// console.log(parse('foo.bar'));
+	it('with attributes', () => {
+		assert.deepEqual(parse('div[foo=bar]'), [
+			['div', 'tag'],
+			['[', 'bracket'],
+			['foo', 'attribute attribute-name'],
+			['=', null],
+			['bar', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('div.a[b=c]'), [
+			['div', 'tag'],
+			['.a', 'attribute class'],
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('div[b=c].a'), [
+			['div', 'tag'],
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket'],
+			['.a', 'attribute class']
+		]);
+
+		assert.deepEqual(parse('div[a=b][c="d"]'), [
+			['div', 'tag'],
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['=', null],
+			['b', 'string-2 attribute-value'],
+			[']', 'bracket'],
+			['[', 'bracket'],
+			['c', 'attribute attribute-name'],
+			['=', null],
+			['"d"', 'string attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[a=b]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['=', null],
+			['b', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('.a[b=c]'), [
+			['.a', 'attribute class'],
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[b=c].a#d'), [
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket'],
+			['.a', 'attribute class'],
+			['#d', 'attribute id']
+		]);
+
+		assert.deepEqual(parse('[b=c]a'), [
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket'],
+			['a', 'tag']
+		]);
+
+		assert.deepEqual(parse('[a]'), [['[', 'bracket'], ['a', 'attribute attribute-name'], [']', 'bracket']]);
+		assert.deepEqual(parse('[a b c]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			[' ', null],
+			['b', 'attribute attribute-name'],
+			[' ', null],
+			['c', 'attribute attribute-name'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[a=b c= d=e]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['=', null],
+			['b', 'string-2 attribute-value'],
+			[' ', null],
+			['c', 'attribute attribute-name'],
+			['=', null],
+			[' ', null],
+			['d', 'attribute attribute-name'],
+			['=', null],
+			['e', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[a=b.c d=тест]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['=', null],
+			['b.c', 'string-2 attribute-value'],
+			[' ', null],
+			['d', 'attribute attribute-name'],
+			['=', null],
+			['тест', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[a="b" c=\'d\' e=""]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['=', null],
+			['"b"', 'string attribute-value'],
+			[' ', null],
+			['c', 'attribute attribute-name'],
+			['=', null],
+			['\'d\'', 'string attribute-value'],
+			[' ', null],
+			['e', 'attribute attribute-name'],
+			['=', null],
+			['""', 'string attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[a="foo\'bar" b=\'foo"bar\' c="foo\\"bar"]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['=', null],
+			['"foo\'bar"', 'string attribute-value'],
+			[' ', null],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['\'foo"bar\'', 'string attribute-value'],
+			[' ', null],
+			['c', 'attribute attribute-name'],
+			['=', null],
+			['"foo\\"bar"', 'string attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[a. b.]'), [
+			['[', 'bracket'],
+			['a', 'attribute attribute-name'],
+			['.', 'operator'],
+			[' ', null],
+			['b', 'attribute attribute-name'],
+			['.', 'operator'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[foo={1 + 2} bar={fn(1, "foo")}]'), [
+			['[', 'bracket'],
+			['foo', 'attribute attribute-name'],
+			['=', null],
+			['{1 + 2}', 'variable-3 attribute-value'],
+			[' ', null],
+			['bar', 'attribute attribute-name'],
+			['=', null],
+			['{fn(1, "foo")}', 'variable-3 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('[name=${1} value=${2:test}]'), [
+			['[', 'bracket'],
+			['name', 'attribute attribute-name'],
+			['=', null],
+			['${1}', 'string-2 attribute-value'],
+			[' ', null],
+			['value', 'attribute attribute-name'],
+			['=', null],
+			['${2:test}', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+	});
+
+	it('with text node', () => {
+		assert.deepEqual(parse('div{foo}'), [['div', 'tag'], ['{', 'bracket'], ['foo', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{foo}'), [['{', 'bracket'], ['foo', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{a b c}'), [['{', 'bracket'], ['a b c', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{a "b c"}'), [['{', 'bracket'], ['a "b c"', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{isn\'t bad}'), [['{', 'bracket'], ['isn\'t bad', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{foo(a => {return "b"});}'), [['{', 'bracket'], ['foo(a => {return "b"});', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{foo(a => {return "b\\}"});}'), [['{', 'bracket'], ['foo(a => {return "b\\}"});', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{foo\\}bar}'), [['{', 'bracket'], ['foo\\}bar', 'text'], ['}', 'bracket']]);
+		assert.deepEqual(parse('{foo\\{bar\\}baz}'), [['{', 'bracket'], ['foo\\{bar\\}baz', 'text'], ['}', 'bracket']]);
+		// assert.deepEqual(parse('{foo\\"}bar}'), [['{', 'bracket'], ['foo\\"}bar', 'text'], ['}', 'bracket']]);
+	});
+
+	it('mixed', () => {
+		assert.deepEqual(parse('div.foo{bar}'), [
+			['div', 'tag'],
+			['.foo', 'attribute class'],
+			['{', 'bracket'],
+			['bar', 'text'],
+			['}', 'bracket']
+		]);
+
+		assert.deepEqual(parse('.foo{bar}#baz'), [
+			['.foo', 'attribute class'],
+			['{', 'bracket'],
+			['bar', 'text'],
+			['}', 'bracket'],
+			['#baz', 'attribute id']
+		]);
+
+		assert.deepEqual(parse('.foo[b=c]{bar}'), [
+			['.foo', 'attribute class'],
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket'],
+			['{', 'bracket'],
+			['bar', 'text'],
+			['}', 'bracket'],
+		]);
+	});
+
+	it('repeated', () => {
+		assert.deepEqual(parse('div.foo*3'), [
+			['div', 'tag'],
+			['.foo', 'attribute class'],
+			['*3', 'number']
+		]);
+
+		assert.deepEqual(parse('.a[b=c]*10'), [
+			['.a', 'attribute class'],
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket'],
+			['*10', 'number']
+		]);
+
+		assert.deepEqual(parse('.a*10[b=c]'), [
+			['.a', 'attribute class'],
+			['*10', 'number'],
+			['[', 'bracket'],
+			['b', 'attribute attribute-name'],
+			['=', null],
+			['c', 'string-2 attribute-value'],
+			[']', 'bracket']
+		]);
+
+		assert.deepEqual(parse('.a*10{text}'), [
+			['.a', 'attribute class'],
+			['*10', 'number'],
+			['{', 'bracket'],
+			['text', 'text'],
+			['}', 'bracket']
+		]);
+
+		assert.deepEqual(parse('*123foo'), [['*123', 'number'], ['foo', 'tag']]);
+		assert.deepEqual(parse('*'), [['*', 'number']]);
+	});
+
+	it('self-closing', () => {
+		assert.deepEqual(parse('div/'), [['div', 'tag'], ['/', 'operator']]);
+		assert.deepEqual(parse('.foo/'), [['.foo', 'attribute class'], ['/', 'operator']]);
+
+		assert.deepEqual(parse('.foo[bar]/'), [
+			['.foo', 'attribute class'],
+			['[', 'bracket'],
+			['bar', 'attribute attribute-name'],
+			[']', 'bracket'],
+			['/', 'operator']
+		]);
+
+		assert.deepEqual(parse('.foo/*3'), [
+			['.foo', 'attribute class'],
+			['/', 'operator'],
+			['*3', 'number']
+		]);
+
+		assert.deepEqual(parse('.foo*3/'), [
+			['.foo', 'attribute class'],
+			['*3', 'number'],
+			['/', 'operator']
+		]);
 	});
 });
