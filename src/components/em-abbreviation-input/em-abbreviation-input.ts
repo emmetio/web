8cd2f3eb8bc47defb55c1dff0f56c702bb5b01d2
Sergey Chikuyonok <serge.che@gmail.com>
2019-11-27 01:36:48 +0300

    Working on preferences

--- src/components/em-abbreviation-input/em-abbreviation-input.ts
+++ src/components/em-abbreviation-input/em-abbreviation-input.ts
@@ -1,81 +1,27 @@
 import { expand } from '@emmetio/expand-abbreviation';
-import resolveConfig from '@emmetio/config';
+import resolveConfig, { EmmetConfig } from '@emmetio/config';
 
-export default {
-	onstate({ changed, current }) {
-		if (changed.expanded) {
-			this._valid = !current.expanded.error;
-			this.fire('change', {
-				value: this.get().value,
-				valid: this._valid
-			});
-		}
-	},
-
-	data() {
-		return {
-			value: '',
-			syntax: 'html',
-			config: {
-				globals: {
-					markup: {
-						profile: {
-							selfClosingStyle: 'xhtml'
-						}
-					}
-				},
-				syntax: {
-					jsx: {
-						options: {
-							jsx: true
-						}
-					}
-				}
-			},
-			syntaxPicker: false,
-			preview: false,
-			error: null
-		};
-	},
-
-	computed: {
-		mime({ $syntaxes, syntax }) {
-			const item = $syntaxes.find(item => item.id === syntax) || $syntaxes[0];
-			return item.mime;
-		},
-
-		expanded: ({ value, syntax, config }) => {
-			try {
-				return {
-					value: value ? expand(value, resolveConfig(config, { syntax })) : ''
-				};
-			} catch (error) {
-				const message = (error.originalMessage || error.message)
-					.replace(/\b(unexpected\s+)(\d+)/, (str, prefix, code) => prefix + String.fromCharCode(+code));
-
-				return {
-					error: {
-						message,
-						line: 0,
-						ch: error.pos,
-					}
-				};
+const defaultConfig: EmmetConfig = {
+	version: 1,
+	globals: {
+		markup: {
+			profile: {
+				selfClosingStyle: 'xhtml'
 			}
 		}
 	},
-
-	methods: {
-		setSyntax(syntax) {
-			this.set({ syntax });
-			this.refs.input.focus();
-		},
-
-		/**
-		 * Check if currently entered abbreviation is valid
-		 * @return {Boolean}
-		 */
-		isValid() {
-			return this._valid;
+	syntax: {
+		jsx: {
+			type: 'markup',
+			options: { jsx: true }
 		}
 	}
 };
+
+interface IEmAbbreviationInputProps {
+	value?: string;
+	syntax?: string;
+	syntaxPicker: boolean;
+	preview: boolean;
+	config?: EmmetConfig;
+}
