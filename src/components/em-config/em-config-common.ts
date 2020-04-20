a65561dd8cc2382d06e5bdbfd63080ede74333bb
Sergey Chikuyonok <serge.che@gmail.com>
2020-04-20 22:13:34 +0300

    Full support of basic Emmet options

--- src/components/em-config/em-config-common.ts
+++ src/components/em-config/em-config-common.ts
@@ -41,6 +41,24 @@ interface CommonConfigForm {
     }
 }
 
+// TODO Move to store?
+/**
+ * Default Emmet config
+ */
+const defaultConfig: EmmetConfig = {
+    mark: true,
+    preview: true,
+    autoRenameTags: true,
+    markTagPairs: true,
+    markupStyle: 'html',
+    attributeQuotes: 'double',
+    previewOpenTag: false,
+    comments: false,
+    commentsTemplate: '\n<!-- /[#ID][.CLASS] -->',
+    bem: false,
+    shortHex: true
+};
+
 export function didChange(component: EmConfigCommon, { options, variables, snippets }: Changes<EmConfigCommonProps>) {
     // In case if any of the incoming props change (which means data was saved),
     // reset local state
@@ -94,17 +112,19 @@ export function willMount(component: EmConfigCommon) {
  */
 export function onChangeOption(component: EmConfigCommon, evt: Event) {
     const elem = evt.target as HTMLInputElement;
+    const name = elem.name;
     const value = elem.type === 'checkbox' ? elem.checked : elem.value;
     const { options, form } = component.state;
 
     const opt: Partial<EmmetConfig> = {
         ...options,
-        [elem.name]: value
-    }
+        [name]: name === 'commentsTemplate' ? unescapeString(value as string) : value
+    };
 
     component.setState({
         options: opt,
         editorConfig: {
+            ...defaultConfig,
             ...component.props.options,
             ...opt
         }
@@ -153,8 +173,8 @@ function createMap(base: EmmetMap, user?: EmmetMap | null): Map<string, string> 
 }
 
 function createOptions(component: EmConfigCommon): ConfigField[] {
-    function value<K extends keyof EmmetConfig>(name: K, defaultValue: EmmetConfig[K]) {
-        return getOptionValue(component, name, defaultValue);
+    function value<K extends keyof EmmetConfig>(name: K) {
+        return getOptionValue(component, name);
     }
 
     return [{
@@ -162,43 +182,43 @@ function createOptions(component: EmConfigCommon): ConfigField[] {
         type: ConfigFieldType.Choose,
         label: 'Output code style',
         items: ['html', 'xhtml', 'xml'],
-        value: value('markupStyle', 'html'),
+        value: value('markupStyle'),
         comment: 'Defines how self-closing tags (like <code>&lt;img&gt;</code> or <code>&lt;br&gt;</code>) and boolean attributes (like <code>disabled</code>) will look in generated code.'
     }, {
         name: 'attributeQuotes',
         label: 'Attribute value quotes',
         type: ConfigFieldType.Choose,
         items: ['double', 'single'],
-        value: value('attributeQuotes', 'double')
+        value: value('attributeQuotes')
     }, {
         name: 'comments',
         type: ConfigFieldType.Boolean,
-        label: 'Enable tag commenting',
-        value: value('comments', false),
+        label: 'Tag comments',
+        value: value('comments'),
         comment: 'If enabled, adds comment with tag details after closing tag of expanded abbreviation element, which contains either <code>id</code> and/or <code>class</code> attribute.',
         children: [{
             name: 'commentsTemplate',
             type: ConfigFieldType.String,
             label: 'Comment template',
-            value: value('commentsTemplate', '\n<!-- /[#ID][.CLASS] -->') as string,
+            value: escapeString(value('commentsTemplate') as string),
             comment: 'Outputs everything between <code>[</code> and <code>]</code> only if specified attribute name (written in UPPERCASE) exists in element. Attribute name is replaced with actual value. Use <code>\n</code> to add a newline.'
         }]
     }, {
         name: 'bem',
         type: ConfigFieldType.Boolean,
         label: 'BEM support',
-        value: value('bem', false),
+        value: value('bem'),
         comment: 'When enabled, Emmet will treat class names starting with <code>-</code> as <em>element</em> and with <code>_</code> as <em>modifier</em> in <a href="https://en.bem.info">BEM</a> notation. These class names will inherit <em>block</em> name from current or ancestor element. For example, the abbreviation <code>ul.nav.nav_secondary>li.nav__item</code> can be shortened to <code>ul.nav._secondary>li.-item</code> with this option enabled.'
     }, {
         name: 'shortHex',
         type: ConfigFieldType.Boolean,
         label: 'Use short HEX colors in CSS',
-        value: value('shortHex', true) as boolean,
+        value: value('shortHex') as boolean,
         comment: 'When enabled, tries to shorten generated HEX color values for CSS abbreviations. For example, <code>c#0</code> abbreviation can be expanded either to <code>color: #000;</code> or <code>color: #000000;</code>'
     }];
 }
 
-function getOptionValue<K extends keyof EmmetConfig>(component: EmConfigCommon, name: K, defaultValue: EmmetConfig[K]): EmmetConfig[K] {
+function getOptionValue<K extends keyof EmmetConfig>(component: EmConfigCommon, name: K): EmmetConfig[K] {
     const { state, props } = component;
     if (state.options && name in state.options) {
         return (state.options as EmmetConfig)[name];
@@ -208,5 +228,40 @@ function getOptionValue<K extends keyof EmmetConfig>(component: EmConfigCommon, 
         return props.options[name];
     }
 
-    return defaultValue;
+    return defaultConfig[name];
+}
+
+const escapeMap = {
+    '\n': '\\n',
+    '\r': '\\r',
+    '\t': '\\t',
+};
+const reverseEscapeMap: { [key: string]: string } = {};
+
+for (const [key, value] of Object.entries(escapeMap)) {
+    reverseEscapeMap[value] = key;
+}
+
+function escapeString(str: string): string {
+    let result = '';
+    for (const ch of str) {
+        result += ch in escapeMap ? escapeMap[ch] : ch;
+    }
+
+    return result;
+}
+
+function unescapeString(str: string): string {
+    let result = '';
+    let pos = 0;
+    while (pos < str.length) {
+        let ch = str[pos++];
+        if (ch === '\\') {
+            ch += str[pos++];
+        }
+
+        result += ch in reverseEscapeMap ? reverseEscapeMap[ch] : ch;
+    }
+
+    return result;
 }
